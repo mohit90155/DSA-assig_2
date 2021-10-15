f158e774f71b9f19dd179748eb043208e1a9b959
mohit90155 <88535553+mohit90155@users.noreply.github.com>
2021-10-15 17:48:19 +0530

    Add files via upload

---
+++ 1_decimal_addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+float x,y;
+printf("Enter first decimal number: ");
+scanf("%f",&x);
+printf("Enter second decimal number: ");
+scanf("%f",&y);
+printf("Sum of above numbers is: %f",x+y);
+    return 0;
+}
